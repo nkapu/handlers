06e7b8c442a9a2e44a94cbd639bb06e7bbc62ccf
Marko Laakso <fenris@iki.fi>
2016-03-18 14:18:29 +0200

    Improved handlers-list.py

--- osx/handlers-list.py
+++ osx/handlers-list.py
@@ -1,10 +1,36 @@
 import os
+import re
 
 dump = os.popen("/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -dump")
 
 # bundle  id:            53140
 #         path:          /Applications/Firefox.app
 #         name:          Firefox
+# ...
+#         claim   id:            36428
+#                 name:          file URL
+#                 rank:          Default
+#                 reqCaps:      
+#                 roles:         Viewer  
+#                 flags:         url-type  
+#                 icon:          
+#                 bindings:      file:
+
+rexps = [
+  re.compile('^\s*(bundle)\s*id:\s*(\d*)'),
+  re.compile('^\s*(path):\s*(.*)'),
+  re.compile('^\s*(name):\s*(.*)'),
+  re.compile('^\s*(claim)\s*id:\s*(\d*)'),
+  re.compile('^\s*(flags):\s*(.*)'),
+  re.compile('^\s*(bindings):\s*(.*)')
+]
+
+handlers = {}
 
 for line in dump.readlines():
-  print line
+  for rexp in rexps:
+    m = rexp.match(line)
+    if m:
+      print m.group(1) + ": " + m.group(2)
+
+dump.close()
